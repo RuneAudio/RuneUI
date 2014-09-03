50b4dcd35eb56490b33009be20c79c1e07b985f9
ACXgit <acx@ngi.it>
2014-09-03 17:16:17 +0200

    Favicons
    Added favicons (generated by Favicon Generator) for:
    - PC and Mac, as usual
    - iPhone and iPad
    - Android phones and tablets
    - Windows 8 tablets
    - And more...

--- app/templates/header.php
+++ app/templates/header.php
@@ -5,8 +5,25 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <link rel="stylesheet" href="<?=$this->asset('/css/runeui.css')?>">
-    <link rel="icon" type="image/x-icon" href="<?=$this->asset('/img/favicon.ico')?>">
-    <link rel="apple-touch-icon" href="<?=$this->asset('/img/apple-touch-icon.png')?>">
+	<link rel="shortcut icon" href="<?=$this->asset('/img/favicon.ico')?>">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?=$this->asset('/img/apple-touch-icon-57x57.png')?>">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?=$this->asset('/img/apple-touch-icon-114x114.png')?>">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?=$this->asset('/img/apple-touch-icon-72x72.png')?>">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?=$this->asset('/img/apple-touch-icon-144x144.png')?>">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?=$this->asset('/img/apple-touch-icon-60x60.png')?>">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?=$this->asset('/img/apple-touch-icon-120x120.png')?>">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?=$this->asset('/img/apple-touch-icon-76x76.png')?>">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?=$this->asset('/img/apple-touch-icon-152x152.png')?>">
+	<meta name="apple-mobile-web-app-title" content="RuneAudio">
+	<link rel="icon" type="image/png" href="<?=$this->asset('/img/favicon-196x196.png')?>" sizes="196x196">
+	<link rel="icon" type="image/png" href="<?=$this->asset('/img/favicon-160x160.png')?>" sizes="160x160">
+	<link rel="icon" type="image/png" href="<?=$this->asset('/img/favicon-96x96.png')?>" sizes="96x96">
+	<link rel="icon" type="image/png" href="<?=$this->asset('/img/favicon-16x16.png')?>" sizes="16x16">
+	<link rel="icon" type="image/png" href="<?=$this->asset('/img/favicon-32x32.png')?>" sizes="32x32">
+	<meta name="msapplication-TileColor" content="#000000">
+	<meta name="msapplication-TileImage" content="/img/mstile-144x144.png">
+	<meta name="msapplication-config" content="/img/browserconfig.xml">
+	<meta name="application-name" content="RuneAudio">
 </head>
 <?php if (empty($this->uri(1)) OR ($this->uri(1) == 'playback')): ?>
 <body id="section-index">
